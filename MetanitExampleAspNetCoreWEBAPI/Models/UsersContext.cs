13237f281f9d482e55ded1dca950d14f815b1dfe
KhusnetdinovDmitrii <d.xusnetd@yandex.ru>
2018-06-09 15:57:44 +0300

    commit

---
+++ MetanitExampleAspNetCoreWEBAPI/Models/UsersContext.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetanitExampleAspNetCoreWEBAPI.Models
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UsersContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
